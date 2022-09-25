a6e5e8b4815fb68a464643f7469d7b3573aaf4bd
Pavel Mogilevskyi <pavel13595@gmail.com>
2022-09-25 21:18:56 +0300

    0.0.1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+console.log(galleryItems);
+
 // Change code below this line
 
-console.log(galleryItems);
+// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+// Реализация делегирования на div.gallery и получение url большого изображения.
+// Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
+// Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
+// Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
+const paletteContainer = document.querySelector(".gallery");
+
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+
+paletteContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+paletteContainer.addEventListener("click", onGalleryClick);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"/>
+    </a>
+  </div>`;
+    })
+    .join("");
+}
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  const isGalleryItem = event.target.classList.contains("gallery__image");
+  if (!isGalleryItem) {
+    return;
+  }
+  const originalImg = event.target.dataset.source;
+  const modal = basicLightbox.create(`
+      <img src="${originalImg}">`);
+  modal.show();
+
+  document.addEventListener("keydown", (event) => {
+    if (event.code !== "Escape") {
+      return;
+    }
+    modal.close();
+    galleryRef.removeEventListener;
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+refs.gallery.innerHTML = createGalleryMarkup(galleryItems);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  caption: true,
+  captionType: "attr",
+  captionsData: "alt",
+  captionDelay: "250ms",
+});
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+                    <img class="gallery__image" 
+                        src="${preview}" 
+                        alt="${description}" />
+                    </a>`;
+    })
+    .join("");
+}
